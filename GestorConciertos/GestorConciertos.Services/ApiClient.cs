1a76133f17c5a4afad5522b976cfcb1e237c41e6
Cristian Kraker <thekraker23@gmail.com>
2025-03-20 18:52:58 +0100

    Correciones de Errores, y mejora de funcionamiento

--- GestorConciertos/GestorConciertos.Services/ApiClient.cs
+++ GestorConciertos/GestorConciertos.Services/ApiClient.cs
@@ -15,12 +15,11 @@ namespace GestorConciertos.Services
         {
             _httpClient = httpClient;
         }
-        public async Task<ResultadoBusqueda> GetBusqueda( string busqueda)
+        public async Task<ResultadoBusqueda> GetBusqueda(string busqueda)
         {
-           
             try
             {
-            var json = await _httpClient.GetStringAsync($"suggest/{busqueda}");
+                var json = await _httpClient.GetStringAsync($"suggest/{busqueda}");
                 ResultadoBusqueda dato = JsonConvert.DeserializeObject<ResultadoBusqueda>(json);
                 return dato;
             }
@@ -30,65 +29,72 @@ namespace GestorConciertos.Services
             }
         }
 
-        public async Task<Letra> GetLetraCancion(string Cancion,string Cantante)
+        public async Task<Letra> GetLetraCancion(string Cancion, string Cantante)
         {
             //"https://api.lyrics.ovh/v1/artist/title";
-            var json = await _httpClient.GetStringAsync($"v1/{Cantante}/{Cancion}");
-            Letra dato = JsonConvert.DeserializeObject<Letra>(json);
-            return dato;
+            try
+            {
+                var json = await _httpClient.GetStringAsync($"v1/{Cantante}/{Cancion}");
+                Letra dato = JsonConvert.DeserializeObject<Letra>(json);
+                return dato;
+            }
+            catch (Exception ex)
+            {
+                return new Letra { Lyrics = "No encontrada" };
+            }
         }
 
     }
 
 
- /*   public class ApiClientfabrixapi : IApiClient
-    {
-        private readonly HttpClient _httpClient;
-        //private const string URL_test = "https://api.lyrics.ovh/v1/artist/title";
+    /*   public class ApiClientfabrixapi : IApiClient
+       {
+           private readonly HttpClient _httpClient;
+           //private const string URL_test = "https://api.lyrics.ovh/v1/artist/title";
 
-        public ApiClient(HttpClient httpClient)
-        {
-            _httpClient = httpClient;
-        }
-        public async Task<ResultApi> GetTest()
-        {
-            //var json = await _httpClient.GetStringAsync(ConfigurationApi.URL_Base+ "/search-song");
-            string url = crearURL();
-            var json = await _httpClient.GetStringAsync(url);
-            ResultApi dato = JsonConvert.DeserializeObject<ResultApi>(json);
-            return dato;
-        }
+           public ApiClient(HttpClient httpClient)
+           {
+               _httpClient = httpClient;
+           }
+           public async Task<ResultApi> GetTest()
+           {
+               //var json = await _httpClient.GetStringAsync(ConfigurationApi.URL_Base+ "/search-song");
+               string url = crearURL();
+               var json = await _httpClient.GetStringAsync(url);
+               ResultApi dato = JsonConvert.DeserializeObject<ResultApi>(json);
+               return dato;
+           }
 
-        public async Task<ResultApi> GetByCancion(string Cancion)
-        {
-            //string peticionCancion = $"{ConfigurationApi.URL}&term={Cancion.Replace(" ", "%20")}&format=json";
-            string peticionCancion = $"search-song";
+           public async Task<ResultApi> GetByCancion(string Cancion)
+           {
+               //string peticionCancion = $"{ConfigurationApi.URL}&term={Cancion.Replace(" ", "%20")}&format=json";
+               string peticionCancion = $"search-song";
 
-            var json = await _httpClient.GetStringAsync(peticionCancion);
-            ResultApi dato = JsonConvert.DeserializeObject<ResultApi>(json);
-            return dato;
-        }
+               var json = await _httpClient.GetStringAsync(peticionCancion);
+               ResultApi dato = JsonConvert.DeserializeObject<ResultApi>(json);
+               return dato;
+           }
 
-        public async Task<ResultApi> GetByCantante(string cantante)
-        {
-            //string peticionCantante = $"{ConfigurationApi.URL}&artist={cantante.Replace(" ", "%20")}&format=json";
-            string peticionCantante = $"search-song";
+           public async Task<ResultApi> GetByCantante(string cantante)
+           {
+               //string peticionCantante = $"{ConfigurationApi.URL}&artist={cantante.Replace(" ", "%20")}&format=json";
+               string peticionCantante = $"search-song";
 
 
-            var json = await _httpClient.GetStringAsync(peticionCantante);
-            ResultApi dato = JsonConvert.DeserializeObject<ResultApi>(json);
-            return dato;
-        }
+               var json = await _httpClient.GetStringAsync(peticionCantante);
+               ResultApi dato = JsonConvert.DeserializeObject<ResultApi>(json);
+               return dato;
+           }
 
-        public string crearURL()
-        {
-            var builder = new UriBuilder(ConfigurationApi.URL_Base + "search-song");
-            builder.Port = -1;
-            var query = HttpUtility.ParseQueryString(builder.Query);
-            query["song_title"] = "Estopa";
-            builder.Query = query.ToString();
-            string url = builder.ToString();
-            return url;
-        }
-    }*/
+           public string crearURL()
+           {
+               var builder = new UriBuilder(ConfigurationApi.URL_Base + "search-song");
+               builder.Port = -1;
+               var query = HttpUtility.ParseQueryString(builder.Query);
+               query["song_title"] = "Estopa";
+               builder.Query = query.ToString();
+               string url = builder.ToString();
+               return url;
+           }
+       }*/
 }
